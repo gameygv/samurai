68b15bff477be825a3fcadccaad7d5479f04652a
[dyad] <gameygv@gmail.com>
2026-02-14 04:13:29 -0600

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

---
+++ FIX_SCHEMA.sql
@@ -0,0 +1,6 @@
+-- =================================================================
+-- FIX: RECARGAR CACHÉ
+-- =================================================================
+-- Ejecuta esto y luego intenta hacer Login de nuevo.
+
+NOTIFY pgrst, 'reload schema';
