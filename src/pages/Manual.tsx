302eff7ee886fc4bcd9a6fca9eecc30449c5a047
[dyad] <gameygv@gmail.com>
2026-02-17 05:12:13 -0600

    [dyad] Fixed JSX Syntax Error in Manual - wrote 1 file(s)

--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -1,3 +1,4 @@
+' character">
 import React from 'react';
 import Layout from '@/components/Layout';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
