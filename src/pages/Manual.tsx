a7b4fe29efc36d6a988d31b4eaccc97ab17859dc
[dyad] <gameygv@gmail.com>
2026-02-18 04:41:50 -0600

    [dyad] Fixed JSX syntax error in Manual.tsx by escaping the '>' character as '&gt;' to ensure successful compilation in Vercel. - wrote 1 file(s)

--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -1,3 +1,4 @@
+' como '>' para asegurar la compilación correcta en Vercel.">
 import React from 'react';
 import Layout from '@/components/Layout';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
